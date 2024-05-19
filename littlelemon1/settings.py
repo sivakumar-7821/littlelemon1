514c96573d06936dd0ab294e66a78af5e3a3c0af
Siva <siva.ak7821@gmail.com>
2024-05-19 23:45:59 +0530

    Second Commit

--- littlelemon1/settings.py
+++ littlelemon1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'littlelemon1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'littlelemon1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon1',
+        'USER': 'root',
+        'PASSWORD':'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'"
+        }
     }
 }
+
 
 
 # Password validation
